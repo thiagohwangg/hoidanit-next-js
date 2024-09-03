f63bed3de7d455d5e9eb60818e297cacee0a216d
thiagohwang <cmnqpart2@gmail.com>
2024-09-03 22:35:11 +0700

    video step 16

--- src/components/layout/admin.header.tsx
+++ src/components/layout/admin.header.tsx
@@ -6,12 +6,11 @@ import { useContext } from 'react';
 import { DownOutlined, SmileOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Dropdown, Space } from 'antd';
-import { useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 
 
-const AdminHeader = () => {
-    const {data: session, status} = useSession()
-    console.log("session: ", session);
+const AdminHeader = (props: any) => {
+    const {session} = props
 
     const { Header } = Layout;
     const { collapseMenu, setCollapseMenu } = useContext(AdminContext)!;
@@ -20,34 +19,15 @@ const AdminHeader = () => {
         {
             key: '1',
             label: (
-                <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-                    1st menu item
-                </a>
+                <span>
+                    Settings
+                </span>
             ),
-        },
-        {
-            key: '2',
-            label: (
-                <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-                    2nd menu item (disabled)
-                </a>
-            ),
-            icon: <SmileOutlined />,
-            disabled: true,
-        },
-        {
-            key: '3',
-            label: (
-                <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-                    3rd menu item (disabled)
-                </a>
-            ),
-            disabled: true,
         },
         {
             key: '4',
             danger: true,
-            label: 'a danger item',
+            label: <span onClick={() => signOut()}>Đăng xuất</span>,
         },
     ];
 
